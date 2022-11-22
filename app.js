6d5c65e0ebf61f3e701dfb1f1aae8badb0a43398
anela <anela89@gmail.com>
2022-11-22 12:19:31 -0800

    Added site-wide 404 and global error handlers

--- app.js
+++ app.js
@@ -10,4 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
+/** handle site-wide 404s */
+app.use(function (req, res) {
+    throw new NotFoundError();
+});
+  
+/** global err handler */
+app.use(function (err, req, res, next) {
+    const status = err.status || 500;
+    const message = err.message;
+    if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+    return res.status(status).json({ error: { message, status } });
+});
+
 module.exports = app;
