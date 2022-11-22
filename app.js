25ded36727ff1fe910bfc34daacee6b5d1b5aebc
anela <anela89@gmail.com>
2022-11-22 12:16:15 -0800

    Import NotFoundError

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 const express = require("express");
 const app = express();
 
+const { NotFoundError } = require("./expressError");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 module.exports = app;
